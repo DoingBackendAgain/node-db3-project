f55cb65e8ee045c289f15b1f831f81130a2f1dad
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-01-10 11:37:48 -0500

    adding notes

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,5 +1,6 @@
 // scheme-model
 const db = require("../data/dbconfig.js")
+const router = require("./scheme-router.js")
 
 module.exports = {
     find,
@@ -25,4 +26,40 @@ function findSteps(id){
         .where("steps.scheme_id", id)
         .select("sc.id", "steps.step_number", "steps.instructions")
         .first("sc.id")
+}
+
+// notes from guided project 
+// SELECT  
+//     c.ContactName CustomerName,
+//     e.FirstName EmployeeFirstName,
+//     e.LastName EmployeeLastName
+// FROM "Order" AS o 
+// LEFT // JOIN Employee AS e On o.EmployeeId = e.id
+// JOIN Customer AS c ON o.CustomerId = c.id
+// WHERE o.Id = 16608;
+
+//different type of joins in squl
+// inner join is the default (sometimes nothing will get returned)
+// left join
+
+
+router.get("users/:id/posts", (req, res, next)=> {
+    try{
+             //after moving to model and creating function
+            //const post = await db.findPostByUserId(req.params.id)
+        const posts =  await db("posts")
+            .innerJoin("users", "users.id", "posts.user_id")
+            .where("posts.user_id", req.params.id)
+            .select("posts.id", "posts.contents", "users.username")
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+function findPostsByUserId(userId){
+    return db("posts")
+        .innerJoin("users", "users.id", "posts.user_id")
+        .where("posts.user_id", userId)
+        .select("posts.id", "posts.contents", "users.username")
 }
